7df1e7e72478ea53af661095394ad75441636a1f
Rollins19-05-2003 <sdeb7433@gmail.com>
2024-11-12 10:55:58 +0530

    first commit

---
+++ backend/utils/conn.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log('MongoDB connection SUCCESS');
+    } catch (error) {
+        console.error('MongoDB connection FAIL');
+        process.exit(1);
+    }
+    };
+
+module.exports = connectDB;
